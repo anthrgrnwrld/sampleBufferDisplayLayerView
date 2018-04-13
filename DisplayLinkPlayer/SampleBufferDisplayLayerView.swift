df9f1a2b10e8a12f78134a5a0eb8ce7a0e381c8e
anthrgrnwrld <anthrgrnwrld@anthrgrnwrldnoMacBook-Air.local>
2018-04-13 20:12:39 +0900

    1st commit

---
+++ DisplayLinkPlayer/SampleBufferDisplayLayerView.swift
@@ -0,0 +1,154 @@
+//
+//  SampleBufferDisplayLayerView.swift
+//  DisplayLinkPlayer
+//
+//  Created by anthrgrnwrld on 2018/03/19.
+//  Copyright © 2018年 anthrgrnwrld. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SampleBufferDisplayLayerView: UIView, AVPlayerItemOutputPullDelegate {
+	
+	override public class var layerClass: Swift.AnyClass {
+		get {
+			return AVSampleBufferDisplayLayer.self
+		}
+	}
+	
+	let player = AVPlayer()
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		player.addObserver(self, forKeyPath: "currentItem", options: [.new, .old, .initial], context: nil)
+		playerItemVideoOutput.setDelegate(self, queue: queue)
+		playerItemVideoOutput.requestNotificationOfMediaDataChange(withAdvanceInterval: advancedInterval)
+		displayLink = CADisplayLink(target: self, selector: #selector(displayLinkCallback(_:)))
+		displayLink.preferredFramesPerSecond = 1 / 30
+		
+		displayLink.isPaused = true
+		displayLink.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)	//タイマーを開始
+	}
+	
+	// KVO
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		switch keyPath {
+		case "currentItem"?:
+			if let item = change![NSKeyValueChangeKey.oldKey] as? AVPlayerItem {
+				item.remove(playerItemVideoOutput)
+			}
+			if let item = change![NSKeyValueChangeKey.newKey] as? AVPlayerItem {
+				item.add(playerItemVideoOutput)
+				videoLayer.controlTimebase = item.timebase
+			}
+		default:
+			super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+		}
+	}
+	
+	
+	// MARK: AVPlayerItemOutputPullDelegate
+	func outputMediaDataWillChange(_ sender: AVPlayerItemOutput) {
+		print("outputMediaDataWillChange")
+		displayLink.isPaused = false
+	}
+	
+	func outputSequenceWasFlushed(_ output: AVPlayerItemOutput) {
+		videoLayer.controlTimebase = player.currentItem?.timebase
+		videoLayer.flush()
+	}
+	
+	//private
+	private let playerItemVideoOutput = AVPlayerItemVideoOutput(pixelBufferAttributes: [kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32ARGB])	// ピクセルフォーマット(32bit BGRA)
+	private let queue = DispatchQueue.main
+	private let advancedInterval: TimeInterval = 0.1
+	private var displayLink: CADisplayLink!
+	private var lastTimestamp: CFTimeInterval = 0
+	
+	private var videoLayer: AVSampleBufferDisplayLayer {
+		return self.layer as! AVSampleBufferDisplayLayer
+	}
+	
+	/**
+	setCADiplayLinkSettingに呼び出されるselector。
+	*/
+	@objc private func displayLinkCallback(_ displayLink: CADisplayLink) {
+		
+		let nextOutputHostTime = displayLink.timestamp + displayLink.duration * CFTimeInterval(displayLink.preferredFramesPerSecond)
+		let nextOutputItemTime = playerItemVideoOutput.itemTime(forHostTime: nextOutputHostTime)
+		if playerItemVideoOutput.hasNewPixelBuffer(forItemTime: nextOutputItemTime) {
+			lastTimestamp = displayLink.timestamp
+			var presentationItemTime = kCMTimeZero
+			let pixelBuffer = playerItemVideoOutput.copyPixelBuffer(forItemTime: nextOutputItemTime, itemTimeForDisplay: &presentationItemTime)
+			displayPixelBuffer(pixelBuffer: pixelBuffer!, atTime: presentationItemTime)
+		} else {
+			if displayLink.timestamp - lastTimestamp > 0.5 {
+				displayLink.isPaused = true
+				playerItemVideoOutput.requestNotificationOfMediaDataChange(withAdvanceInterval: advancedInterval)
+			}
+		}
+		
+	}
+
+	private var videoInfo: CMVideoFormatDescription?
+	//private var videoInfo: Unmanaged<CMVideoFormatDescription>?
+	@objc private func displayPixelBuffer(pixelBuffer: CVPixelBuffer, atTime outputTime: CMTime) {
+		
+		var err: OSStatus = noErr
+		
+		if videoInfo == nil {
+			//let unmanagedVideoInfo: Unmanaged<CMVideoFormatDescription?> = Unmanaged<CMVideoFormatDescription?>.passUnretained(videoInfo)
+			
+			//let videoInfoRawPointer: UnsafeMutableRawPointer? = unmanagedVideoInfo.toOpaque()
+//			let videoInfoRawPointer: UnsafeMutableRawPointer = unsafeBitCast(videoInfo, to: UnsafeMutableRawPointer.self)
+//			let videoInfoOpaquePointer: OpaquePointer? = OpaquePointer(videoInfoRawPointer)
+//			let videoInfoPointer: UnsafeMutablePointer<CMVideoFormatDescription?> = UnsafeMutablePointer<CMVideoFormatDescription?>(videoInfoOpaquePointer!)
+			err = CMVideoFormatDescriptionCreateForImageBuffer(nil, pixelBuffer, &videoInfo)
+
+			if (err != noErr) {
+				print("Error at CMVideoFormatDescriptionCreateForImageBuffer \(err)")
+			}
+			
+		}
+
+//		if videoInfo == nil || false == CMVideoFormatDescriptionMatchesImageBuffer((videoInfo?.takeUnretainedValue())!, pixelBuffer) {
+//			if videoInfo != nil {
+//				videoInfo?.release()
+//				videoInfo = nil
+//			}
+//
+//			let videoInfoRawPointer: UnsafeMutableRawPointer? = videoInfo?.toOpaque()
+//			let videoInfoOpaquePointer: OpaquePointer? = OpaquePointer(videoInfoRawPointer)
+//			let videoInfoPointer: UnsafeMutablePointer<CMVideoFormatDescription?> = UnsafeMutablePointer<CMVideoFormatDescription?>(videoInfoOpaquePointer!)
+//			err = CMVideoFormatDescriptionCreateForImageBuffer(nil, pixelBuffer, videoInfoPointer)
+//
+//			if (err != noErr) {
+//				NSLog("Error at CMVideoFormatDescriptionCreateForImageBuffer \(err)")
+//			}
+//		}
+		
+		var sampleTimingInfo = CMSampleTimingInfo(duration: kCMTimeInvalid, presentationTimeStamp: outputTime, decodeTimeStamp: kCMTimeInvalid)
+//		let unmanagedSampleTimingInfo = Unmanaged<AnyObject>.passUnretained(sampleTimingInfo as AnyObject)
+//		let sampleTimingInfoRawPointer = unmanagedSampleTimingInfo.toOpaque()
+//		let sampleTimingInfoOpaquePointer = OpaquePointer(sampleTimingInfoRawPointer)
+//		let sampleTimingInfoPointer = UnsafeMutablePointer<CMSampleTimingInfo>(sampleTimingInfoOpaquePointer)
+
+		var sampleBuffer: CMSampleBuffer?
+		
+//		var sampleBuffer: Unmanaged<CMSampleBuffer>?
+//		let sampleBufferRawPointer = unmanagedSampleTimingInfo.toOpaque()
+//		let sampleBufferOpaquePointer = OpaquePointer(sampleBufferRawPointer)
+//		let sampleBufferPointer = UnsafeMutablePointer<CMSampleBuffer?>(sampleBufferOpaquePointer)
+		
+		err = CMSampleBufferCreateForImageBuffer(nil, pixelBuffer, true, nil, nil, videoInfo!, &sampleTimingInfo, &sampleBuffer)
+		if (err != noErr) {
+			NSLog("Error at CMSampleBufferCreateForImageBuffer \(err)")
+		}
+		
+		if videoLayer.isReadyForMoreMediaData {
+			videoLayer.enqueue(sampleBuffer!)
+		}
+		sampleBuffer = nil
+	}
+}
--- DisplayLinkPlayer/ViewController.swift
+++ DisplayLinkPlayer/ViewController.swift
@@ -7,19 +7,145 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVPlayerItemOutputPullDelegate {
+	
+	var player: AVPlayer {
+		return (view as! SampleBufferDisplayLayerView).player
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		checkPermission()
+		
 	}
-
+	
+	// playItemEndTimeObserving
+	var playItemEndTimeObserver: AnyObject! {
+		didSet {
+			if oldValue != nil && !oldValue.isEqual(playItemEndTimeObserver) {
+				NotificationCenter.default.removeObserver(oldValue)
+			}
+		}
+	}
+	var playItemDidReachEndTime = false
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		playItemEndTimeObserver = NotificationCenter.default.addObserver(
+			forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+			object: nil,
+			queue: nil) {
+				note in
+				if note.object as? AVPlayerItem == self.player.currentItem {
+					self.playItemDidReachEndTime = true
+				}
+		}
+		
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		playItemEndTimeObserver = nil
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	func checkPermission() {
+		let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+		switch photoAuthorizationStatus {
+		case .authorized:
+			print("Access is granted by user")
+		case .notDetermined:
+			PHPhotoLibrary.requestAuthorization({
+				(newStatus) in
+				print("status is \(newStatus)")
+				if newStatus ==  PHAuthorizationStatus.authorized {
+					/* do stuff here */
+					print("success")
+				}
+			})
+			print("It is not determined until now")
+		case .restricted:
+			// same same
+			print("User do not have access to photo album.")
+		case .denied:
+			// same same
+			print("User has denied the permission.")
+		}
+	}
+	
+	@IBAction func pressMovieLibraryButton(_ sender: Any) {
+		
+		//引数よりSourceTypeを指定
+		var pickerSourceType: UIImagePickerControllerSourceType
+		pickerSourceType = .photoLibrary
+		
+		//Libraryにアクセス出来るか確認. 出来なければreturn.
+		guard UIImagePickerController.isSourceTypeAvailable(pickerSourceType) else {
+			print("Cannot access PickerControllerSourceType.")
+			return
+		}
+		
+		let pickerController = UIImagePickerController()   //ImagePickerControllerをインスタンス化
+		pickerController.delegate = self                   //delegateを自身に設定
+		//pickerController.sourceType = pickerSourceType     //カメラとライブラリどちらを表示するか指定
+		pickerController.sourceType = .savedPhotosAlbum
+		pickerController.mediaTypes = ["public.movie"]
+		pickerController.videoQuality = .typeHigh
+		pickerController.modalPresentationStyle = .popover
+		
+		pickerController.modalPresentationStyle = .popover
+		let popPC = pickerController.popoverPresentationController
+		popPC?.permittedArrowDirections = .any
+		popPC?.barButtonItem = self.navigationItem.rightBarButtonItem
+		
+		DispatchQueue.main.async() {
+			self.present(pickerController, animated: true, completion: nil)   //imagePickerControllerに遷移
+		}
+	}
+	
+	@objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		
+		self.dismiss(animated: true, completion: nil)
+		
+		if let videoURL = info["UIImagePickerControllerReferenceURL"] as? URL {
+			print("\(NSStringFromClass(self.classForCoder)).\(#function) is called!")
+			//let item = AVPlayerItem(url: videoURL)
+			let asset = AVURLAsset(url: videoURL)
+			let item = AVPlayerItem(asset: asset)
+			player.replaceCurrentItem(with: item)
+		}
+		
+		
+		
+	}
+
+	@IBAction func pressTogglePlayPause(_ sender: Any) {
+
+		if player.rate == 0.0 {
+			let hostTimeNow = CMClockGetTime(CMClockGetHostTimeClock())
+			let hostTimeDelta = CMTimeMakeWithSeconds(0.01, hostTimeNow.timescale)
+			if playItemDidReachEndTime {
+				playItemDidReachEndTime = false
+				//player.setRate(1.0, time: kCMTimeZero, atHostTime: hostTimeNow)
+				player.play()// だと再度再生できない。
+			} else {
+				//player.setRate(1.0, time: kCMTimeInvalid, atHostTime: hostTimeNow)
+				player.play()
+			}
+		} else {
+			player.pause()
+		}
+		
+	}
+	
 
 
 }
-
